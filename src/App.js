b93966dc08c12053891b3ff0446c7f76cdae9b60
huynhn <huynhn.vndigitech@gmail.com>
2023-11-15 02:38:06 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -2,22 +2,50 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const name = "Huy";
+  const age = 18;
+  const isMale = true;
+  const student = {
+    name: "Nhat Huy",
+  };
+  const colorList = ["red", "green", "blue"];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Xin chao {name} - {age} - {isMale ? "Male" : "Female"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Test Vercel 2</h1>
+
+        {isMale ? <p>Male</p> : <p>Female</p>}
+
+        {isMale && <p>Male</p>}
+        {!isMale && <p>Female</p>}
+
+        {isMale && (
+          <div>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </div>
+        )}
+
+        {isMale && (
+          <>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </>
+        )}
+
+        <p>{student.name}</p>
+
+        <ul>
+          {colorList.map((color) => (
+            <li style={{ color }}>{color}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
